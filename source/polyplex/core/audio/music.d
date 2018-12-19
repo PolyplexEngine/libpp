452db1ddafd7f240c2aab79491bdfc082c5e2562
Member1221 <clipseypone@gmail.com>
2018-12-19 02:22:23 +0100

    small (ugly) preperation for multi-channel music playback

--- source/polyplex/core/audio/music.d
+++ source/polyplex/core/audio/music.d
@@ -11,33 +11,42 @@ import core.thread;
 import core.time;
 import core.sync.mutex;
 
-private 	__gshared Music iMus;
-private 	__gshared size_t totalProcessed;
-private 	__gshared ALuint deqBuff;
-private 	__gshared Thread musicThread;
 protected 	__gshared bool shouldStop;
+private     __gshared playback[] playbackThreads;
 private		__gshared bool started;
-private		__gshared byte[] streamBuffer;
-private     __gshared bool songEnded;
 
-protected void MusicHandlerThread() {
+struct playback {
+    private 	__gshared Music iMusPtr;
+    private 	__gshared size_t totalProcessed;
+    private 	__gshared Tid musicThread;
+    private		__gshared bool started;
+}
+
+protected void MusicHandlerThread(int channel) {
 	int buffersProcessed;
+    bool songEnded;
+    ALuint deqBuff;
     ALint state;
+    byte[] streamBuffer = new byte[playbackThreads[channel].iMusPtr.bufferSize];
     try {
 	    while (!shouldStop) {
             Thread.sleep(10.msecs);
+
+            Music iMus = playbackThreads[channel].iMusPtr;
             if (iMus is null) continue;
+			streamBuffer.length = iMus.bufferSize;
 
             alGetSourcei(iMus.source, AL_BUFFERS_PROCESSED, &buffersProcessed);
-            totalProcessed += buffersProcessed;
+            playbackThreads[channel].totalProcessed += buffersProcessed;
+
             while(buffersProcessed > 0) {
                 deqBuff = 0;
                 alSourceUnqueueBuffers(iMus.source, 1, &deqBuff);
-                
+
                 // Read audio from stream in
                 size_t readData = iMus.stream.read(streamBuffer.ptr, iMus.bufferSize);
                 if (readData == 0) {
-                    if (iMus.IsLooping) {
+                    if (iMus.looping) {
                         iMus.stream.seek;
                         readData = iMus.stream.read(streamBuffer.ptr, iMus.bufferSize);
                     } else {
@@ -69,9 +78,11 @@ protected void MusicHandlerThread() {
 }
 
 protected void spawnMusicHandler() {
+    if (started) return;
 	started = true;
-	musicThread = new Thread(&MusicHandlerThread);
-	musicThread.start();
+    playback pb;
+    pb.musicThread = spawn(&MusicHandlerThread, 0);
+    playbackThreads ~= pb;
 }
 
 public class Music {
@@ -83,6 +94,8 @@ private:
 
     int bufferSize;
     int buffers;
+
+    private bool looping;
 
     // Source
     ALuint source;
@@ -108,7 +121,6 @@ public:
         buffer = new ALuint[buffers];
 
         this.bufferSize = cast(int)stream.info.bitrate*stream.info.channels;
-        streamBuffer = new byte[bufferSize];
 
         alGenBuffers(buffers, buffer.ptr);
 
@@ -122,7 +134,15 @@ public:
 		if (!started) spawnMusicHandler();
     }
 
+    ~this() {
+        // Cleanup procedure
+        alDeleteSources(1, &source);
+        alDeleteBuffers(buffers, buffer.ptr);
+    }
+
     private void prestream() {
+		byte[] streamBuffer = new byte[bufferSize];
+
         // Pre-read some data.
         foreach (i; 0 .. buffers) {
 			// Read audio from stream in
@@ -134,17 +154,22 @@ public:
         }
     }
     
+	public @property bool Looping() {
+		return looping;
+	}
+	public @property void Looping(bool val) { looping = val; }
+
     void Play() {
 		synchronized {
-			if (iMus !is null) iMus.Stop();
-			iMus = this;
+			if (playbackThreads[0].iMusPtr !is null) playbackThreads[0].iMusPtr.Stop();
+			playbackThreads[0].iMusPtr = this;
 			alSourcePlay(source);
 		}
     }
 
     void Stop() {
 		synchronized {
-			iMus = null;
+			playbackThreads[0].iMusPtr = null;
 			alSourceStop(source);
 			stream.seek();
 			prestream();
@@ -156,10 +181,134 @@ public:
 			return stream.tell;
 		}
     }
+	
+	/*
+		PITCH
+	*/
+	public @property float Pitch() {
+		float v = 0f;
+		alGetSourcef(source, AL_PITCH, &v);
+		return v;
+	}
+	public @property void Pitch(float val) { alSourcef(source, AL_PITCH, val); }
 
-    bool isThreadRunning() {
-        return musicThread.isRunning;
-    }
+	/*
+		GAIN
+	*/
+	public @property float Gain() {
+		float v = 0f;
+		alGetSourcef(source, AL_GAIN, &v);
+		return v;
+	}
+	public @property void Gain(float val) { alSourcef(source, AL_GAIN, val); }
 
-    bool IsLooping;
+	/*
+		MIN GAIN
+	*/
+	public @property float MinGain() {
+		float v = 0f;
+		alGetSourcef(source, AL_MIN_GAIN, &v);
+		return v;
+	}
+	public @property void MinGain(float val) { alSourcef(source, AL_MIN_GAIN, val); }
+
+	/*
+		MAX GAIN
+	*/
+	public @property float MaxGain() {
+		float v = 0f;
+		alGetSourcef(source, AL_MAX_GAIN, &v);
+		return v;
+	}
+	public @property void MaxGain(float val) { alSourcef(source, AL_MAX_GAIN, val); }
+
+	/*
+		MAX DISTANCE
+	*/
+	public @property float MaxDistance() {
+		float v = 0f;
+		alGetSourcef(source, AL_MAX_DISTANCE, &v);
+		return v;
+	}
+	public @property void MaxDistance(float val) { alSourcef(source, AL_MAX_DISTANCE, val); }
+
+	/*
+		ROLLOFF FACTOR
+	*/
+	public @property float RolloffFactor() {
+		float v = 0f;
+		alGetSourcef(source, AL_ROLLOFF_FACTOR, &v);
+		return v;
+	}
+	public @property void RolloffFactor(float val) { alSourcef(source, AL_ROLLOFF_FACTOR, val); }
+
+	/*
+		CONE OUTER GAIN
+	*/
+	public @property float ConeOuterGain() {
+		float v = 0f;
+		alGetSourcef(source, AL_CONE_OUTER_GAIN, &v);
+		return v;
+	}
+	public @property void ConeOuterGain(float val) { alSourcef(source, AL_CONE_OUTER_GAIN, val); }
+
+	/*
+		CONE INNER ANGLE
+	*/
+	public @property float ConeInnerAngle() {
+		float v = 0f;
+		alGetSourcef(source, AL_CONE_INNER_ANGLE, &v);
+		return v;
+	}
+	public @property void ConeInnerAngle(float val) { alSourcef(source, AL_CONE_INNER_ANGLE, val); }
+
+	/*
+		CONE OUTER ANGLE
+	*/
+	public @property float ConeOuterAngle() {
+		float v = 0f;
+		alGetSourcef(source, AL_CONE_OUTER_ANGLE, &v);
+		return v;
+	}
+	public @property void ConeOuterAngle(float val) { alSourcef(source, AL_CONE_OUTER_ANGLE, val); }
+
+	/*
+		REFERENCE DISTANCE
+	*/
+	public @property float ReferenceDistance() {
+		float v = 0f;
+		alGetSourcef(source, AL_REFERENCE_DISTANCE, &v);
+		return v;
+	}
+	public @property void ReferenceDistance(float val) { alSourcef(source, AL_REFERENCE_DISTANCE, val); }
+
+	/*
+		POSITION
+	*/
+	public @property Vector3 Position() {
+		Vector3 v = 0f;
+		alGetSourcefv(source, AL_POSITION, v.ptr);
+		return v;
+	}
+	public @property void Position(Vector3 val) { alSourcefv(source, AL_POSITION, val.ptr); }
+
+	/*
+		VELOCITY
+	*/
+	public @property Vector3 Velocity() {
+		Vector3 v = 0f;
+		alGetSourcefv(source, AL_VELOCITY, v.ptr);
+		return v;
+	}
+	public @property void Velocity(Vector3 val) { alSourcefv(source, AL_VELOCITY, val.ptr); }
+
+	/*
+		DIRECTION
+	*/
+	public @property Vector3 Direction() {
+		Vector3 v = 0f;
+		alGetSourcefv(source, AL_DIRECTION, v.ptr);
+		return v;
+	}
+	public @property void Direction(Vector3 val) { alSourcefv(source, AL_DIRECTION, val.ptr); }
 }
