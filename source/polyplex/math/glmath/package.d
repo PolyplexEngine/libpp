a8281d81441fb14f1c0fd022f5e4395eed33aead
Member1221 <clipseypone@gmail.com>
2019-08-10 11:38:05 +0200

    Remove GL_MATH, SIMPLE_MATH is now main impl, will be rewritten soon.

--- source/polyplex/math/glmath/package.d
+++
@@ -1,5 +0,0 @@
-module polyplex.math.glmath;
-public import polyplex.math.glmath.matrix;
-public import polyplex.math.glmath.vector;
-public import polyplex.math.glmath.quaternion;
-public import polyplex.math.glmath.geometry;
--- source/polyplex/math/package.d
+++ source/polyplex/math/package.d
@@ -3,8 +3,4 @@ module polyplex.math;
 public static import Mathf = polyplex.math.mathf;
 public import polyplex.math.rectangle;
 
-version(GL_MATH) {
-	public import polyplex.math.glmath;
-} else {
-	public import polyplex.math.simplemath;
-}
+public import polyplex.math.glmath;
