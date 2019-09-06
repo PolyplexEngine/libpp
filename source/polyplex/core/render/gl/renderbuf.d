ff85995d5bc78d49f7772929fcdb5312652ccdd8
Member1221 <clipseypone@gmail.com>
2019-09-06 02:58:12 +0200

    Fix framebuffer, optimize it as well

--- source/polyplex/core/render/gl/renderbuf.d
+++ source/polyplex/core/render/gl/renderbuf.d
@@ -4,7 +4,22 @@ import polyplex.core.render.gl.gloo;
 import polyplex.core.render.gl.gloo : RBO = Renderbuffer, FBO = Framebuffer;
 
 enum Attachment {
-    Color=GL_COLOR_ATTACHMENT0,
+    Color0=GL_COLOR_ATTACHMENT0,
+    Color1=GL_COLOR_ATTACHMENT1,
+    Color2=GL_COLOR_ATTACHMENT2,
+    Color3=GL_COLOR_ATTACHMENT3,
+    Color4=GL_COLOR_ATTACHMENT4,
+    Color5=GL_COLOR_ATTACHMENT5,
+    Color6=GL_COLOR_ATTACHMENT6,
+    Color7=GL_COLOR_ATTACHMENT7,
+    Color8=GL_COLOR_ATTACHMENT8,
+    Color9=GL_COLOR_ATTACHMENT9,
+    Color10=GL_COLOR_ATTACHMENT10,
+    Color11=GL_COLOR_ATTACHMENT11,
+    Color12=GL_COLOR_ATTACHMENT12,
+    Color13=GL_COLOR_ATTACHMENT13,
+    Color14=GL_COLOR_ATTACHMENT14,
+    Color15=GL_COLOR_ATTACHMENT15,
     Depth=GL_DEPTH_ATTACHMENT
 }
 
@@ -27,6 +42,7 @@ private:
             to.Image2D(TextureType.Tex2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, null);
             to.SetParameter(TextureType.Tex2D, TextureParameter.MagFilter, GL.Nearest);
             to.SetParameter(TextureType.Tex2D, TextureParameter.MinFilter, GL.Nearest);
+            to.Unbind();
         }
 
         rbo.Bind(); 
@@ -37,6 +53,7 @@ private:
         foreach(i, to; renderTextures) {
             to.Bind(TextureType.Tex2D);
             fbo.Texture(GL_FRAMEBUFFER, drawBufs[i], to.Id, 0);
+            to.Unbind();
         }
         GL.DrawBuffers(cast(int)drawBufs.length, drawBufs.ptr);
     }
@@ -61,11 +78,10 @@ public:
         fbo = new FBO();
         rbo = new RBO();
 
-        int colors = 0;
+        // Add color attachment textures
         foreach(i; 0..colorAttachments) {
-            drawBufs ~= GL_COLOR_ATTACHMENT0+colors;
+            drawBufs ~= GL_COLOR_ATTACHMENT0+cast(uint)i;
             renderTextures ~= new Texture();
-            colors++;
         }
 
         // Setup framebuffer
@@ -82,7 +98,11 @@ public:
         return renderTextures;
     }
 
-    void Resize(int width, int height, int colorAttachments = 1) {
+    /**
+        Resize destroys the current framebuffer and recreates it with the specified size.
+    */
+    void Resize(int width, int height) {
+        immutable(size_t) colorAttachments = renderTextures.length;
         destroy(rbo);
         destroy(fbo);
         destroy(renderTextures);
@@ -96,14 +116,9 @@ public:
 
         // TODO: Optimize this.
         renderTextures = new Texture[colorAttachments];
-        if (colorAttachments != drawBufs.length) {
-            drawBufs = [];
-            int colors;
-            foreach(i; 0..colorAttachments) {
-                drawBufs ~= GL_COLOR_ATTACHMENT0+colors;
-                renderTextures[i] = new Texture();
-                colors++;
-            }
+        foreach(i; 0..colorAttachments) {
+            drawBufs ~= GL_COLOR_ATTACHMENT0+cast(uint)i;
+            renderTextures[i] = new Texture();
         }
 
         // Setup framebuffer
@@ -112,16 +127,12 @@ public:
     }
 
     void Begin() {
-        //destroy(rbo);
-        //destroy(to);
-        //to  = new Texture();
-        //rbo = new Renderbuffer();
-        fbo.Bind(FramebufferType.Multi);
-        bufferTexture();
+        fbo.Bind();
         GL.Viewport(0, 0, width, height);
     }
 
-    static void End() {
+    void End() {
         GL.BindFramebuffer(GL_FRAMEBUFFER, 0);
+        fbo.Unbind();
     }
 }
